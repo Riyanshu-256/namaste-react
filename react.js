286efc7a09d150802f9d8054791da4391f76a762
Anshu <riyanshu062@gmail.com>
2025-12-11 07:15:01 +0530

    Inception

--- react.js
+++ react.js
@@ -6,10 +6,18 @@
     </div>
 </div>
 */
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, React.createElement("h1", {}, "Heyyyyy Riyanshu Sharma! 😁")));  // {} => This is the palace where you will give attribute to the tag (id, class)
-console.log(parent);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Heyyyyy Riyanshu Sharma! 😁")
+  )
+); // {} => This is the palace where you will give attribute to the tag (id, class)
+console.log(parent); // give object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
