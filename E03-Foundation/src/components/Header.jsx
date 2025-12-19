322ef5521a3471830c6bb301e625e9a1ba0d915a
Anshu <riyanshu062@gmail.com>
2025-12-19 22:20:48 +0530

    Render Login logout

--- E03-Foundation/src/components/Header.jsx
+++ E03-Foundation/src/components/Header.jsx
@@ -2,9 +2,12 @@
 // like Home, About, and Cart. It appears on every page.
 
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 // Header Component
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -22,6 +25,16 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
