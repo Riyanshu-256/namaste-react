47d31a9e047aee494d79fa463c8538fe19f656f7
Anshu <riyanshu062@gmail.com>
2025-12-20 12:17:52 +0530

    Create Router Page

--- E03-Foundation/src/app.jsx
+++ E03-Foundation/src/app.jsx
@@ -4,6 +4,10 @@
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 // Applayout Component contains all components of planning
 const AppLayout = () => {
@@ -15,6 +19,22 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
