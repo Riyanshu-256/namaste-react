74ba4ffafe11aa482f3cae759309fe345f4bcac7
Anshu <riyanshu062@gmail.com>
2025-12-21 22:35:25 +0530

    Grocery

--- E03-Foundation/src/app.jsx
+++ E03-Foundation/src/app.jsx
@@ -1,6 +1,7 @@
 // THIS FILE STORES REACT CODE
 // app.jsx is the main file of the app. It joins all components together and decides the basic layout of the application.
 
+import { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -9,6 +10,10 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Grocery from "./components/Grocery";
+import Shimmer from "./components/Shimmer";
+
+const Grocery = lazy(() => import("./components/Grocery"));
 
 // Applayout Component contains all components of planning
 const AppLayout = () => {
@@ -36,6 +41,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resId",
